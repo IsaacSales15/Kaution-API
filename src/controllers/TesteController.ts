829cb5214ede3f60629f98608e3859c047efb076
Kayo Lucas Lima <kayodomine@gmail.com>
2024-08-07 14:26:59 -0300

    mongo
    mongol

--- src/controllers/TesteController.ts
+++ src/controllers/TesteController.ts
@@ -3,3 +3,7 @@ import {Request, Response} from "express";
 export const test = async (req: Request, res: Response) => {
     return res.status(200).json({message: "Qualquer Coisa"});
 }
+
+export const testpost = async (req: Request, res: Response) => {
+    return res.status(200).json({message: "Qualquer Coisa"});
+}
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { test } from "./controllers/TesteController";
+import { test, testpost } from "./controllers/TesteController";
 
 export const router = Router();
 
 router.get("/test", test);
+router.post("/test", testpost)
