e545fa0cd0fe8a28105312d657fce22ada26bc98
Matusalém <matusalemdedeus@gmail.com>
2024-08-18 21:52:33 -0300

    novamente tentando resolver isso

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
 import express from "express";
 import { router } from "./router";
-import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(router);
-app.use(
-  cors({
-    origin: "https://kaution-client-side.vercel.app/",
-    allowedHeaders: ["*"],
-    credentials: true,
-  })
-);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
--- src/router.ts
+++ src/router.ts
@@ -1,32 +1,21 @@
 import { Router } from "express";
-import {
-  categoryGet,
-  categoryPost,
-  categoryDelete,
-  categoryPut,
-} from "./controllers/category-controllers/CategoryController";
-import {
-  createUser,
-  loginUser,
-} from "./controllers/register-controllers/UserController";
-import {
-  getUsers,
-  getSingleUser,
-  deleteUser,
-  updateUser,
-  updatePassword,
-} from "./controllers/register-controllers/UserActionsController";
-import {
-  productPost,
-  productGet,
-  productDelete,
-  productPut,
-} from "./controllers/product-controllers/ProductsController";
+import { categoryGet, categoryPost, categoryDelete, categoryPut } from "./controllers/category-controllers/CategoryController";
+import { createUser, loginUser } from "./controllers/register-controllers/UserController";
+import { getUsers, getSingleUser, deleteUser, updateUser, updatePassword } from "./controllers/register-controllers/UserActionsController";
+import { productPost, productGet, productDelete, productPut } from "./controllers/product-controllers/ProductsController";
 import { verify } from "./controllers/register-controllers/VerifyController";
 import { resendCode } from "./controllers/register-controllers/ResendCodeController";
 import { authMiddleware } from "./middlewares/AuthMiddleware";
+import cors from "cors";
 
 export const router = Router();
+
+// Enable CORS from front-end origin
+router.use(cors({
+  origin: "https://kaution-client-side.vercel.app/",
+  allowedHeaders: ["*"],
+  credentials: true,
+}))
 
 //categories
 // se o front quiser pegar todas as categorias, ele passa 'all' no parâmetro 'userid'.
@@ -48,11 +37,7 @@ router.post("/user/user-login", loginUser);
 router.get("/user/user-getAll", getUsers);
 router.get("/user/user-profile/:userId", authMiddleware(), getSingleUser);
 router.put("/user/user-profile/put-user/:userId", authMiddleware(), updateUser);
-router.put(
-  "/user/user-profile/put-password/:userId",
-  authMiddleware(),
-  updatePassword
-);
+router.put("/user/user-profile/put-password/:userId", authMiddleware(), updatePassword);
 router.delete("/user/user-delete/:userId", deleteUser);
 
 //verify
