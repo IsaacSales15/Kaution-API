024f789fe6efe5cb5a8a2e5eb3b251352d236382
Isaac Sales Rodrigues <124395470+IsaacSales15@users.noreply.github.com>
2024-08-25 14:23:57 -0300

    Atualizações no EmailService

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -1,26 +1,46 @@
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const OAuth2Client = new google.auth.OAuth2(
+  process.env.OAUTH_CLIENTID,
+  process.env.OAUTH_CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+OAuth2Client.setCredentials({ refresh_token: process.env.OAUTH_REFRESH_TOKEN });
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, 
     auth: {
-        type: "OAuth2",
-        user: process.env.MAIL_USERNAME,
-        clientId: process.env.OAUTH_CLIENTID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      type: "OAuth2",
+      user: process.env.MAIL_USERNAME,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      accessToken: OAuth2Client.getAccessToken().then(tokenResponse => {
+        if (tokenResponse && tokenResponse.token) {
+          return tokenResponse.token;
+        }
+        throw new Error("Failed to retrieve access token");
+      }),
     },
     tls: {
-        rejectUnauthorized: false
-    }
-});
+      rejectUnauthorized: false,
+    },
+  } as nodemailer.TransportOptions);
 
 export const sendEmail = async (email: string, name: string, code: string) => {
-    const mailOptions = {
-        from: process.env.MAIL_USERNAME,
-        to: email,
-        subject: "Bem vindo ao Kaution! Use seu codigo de verificação.",
-        text: `Olá, ${name}! Seu código: ${code}`
-    }
+  const mailOptions = {
+    from: process.env.MAIL_USERNAME,
+    to: email,
+    subject: "Bem vindo ao Kaution! Use seu codigo de verificação.",
+    text: `Olá, ${name}! Seu código: ${code}`,
+  };
 
-    return transporter.sendMail(mailOptions);
-}
+  return transporter.sendMail(mailOptions);
+};
