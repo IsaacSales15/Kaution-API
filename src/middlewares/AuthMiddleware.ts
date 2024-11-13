84b1f91c97d05248d2d98284e872313401981a03
Isaac Sales Rodrigues <124395470+IsaacSales15@users.noreply.github.com>
2024-11-13 00:27:15 -0300

    Permissions system update

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -5,19 +5,15 @@ import { prisma } from "../database/prisma";
 interface DecodedToken {
   userId: string;
 }
-
-//Não sei se coloco um sistema de permissões, mas vou deixar aqui caso eu tenha coragem de fazer
 export function authMiddleware(permissions?: string[]) {
   return async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ error: "Unauthorized" });
     }
-
     const token = authHeader.substring(7);
     try {
       const JWT_SECRET = process.env.JWT_SECRET;
-
       if (!JWT_SECRET) {
         return res.status(500).json({ error: "Internal server error" });
       }
@@ -25,33 +21,21 @@ export function authMiddleware(permissions?: string[]) {
       const decoded = verify(token, JWT_SECRET) as DecodedToken;
       req.user = { userId: decoded.userId };
 
-    if(permissions) {
-      const user = await prisma.user.findUnique({
-        where: {
-          id: decoded.userId
-        },
-        include: {
-          UserAccess: {
-            select: {
-              Access: {
-                select: {
-                  name: true
-                }
-              }
-            }
-          }
+      if(permissions){
+        const inventoryId = req.params.inventoryId;
+        const userRole = await prisma.inventoryAccess.findFirst({
+          where: {userId: decoded.userId, inventoryId},
+          select: {role: true}
+        });
+
+        if(!userRole || !permissions.includes(userRole.role)){
+          return res.status(403).json({ error: "Permission denied" });
         }
-      });
-      const userPermissions = user?.UserAccess.map((na) => na.Access?.name) ?? [];
-      const hasPermissions = permissions.some((p) =>
-        userPermissions.includes(p))
-      //Continuar essa merda
-    }
+      }
 
-      return next();
-
+      next();
     } catch (error) {
-      console.log(error);
+      console.error(error);
       return res.status(500).json({ error: "Internal server error" });
     }
   };
